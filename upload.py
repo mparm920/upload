31b37a48fb1a32014e4c20185efb4cd86d8f1a28
Mark <mparm920@gmail.com>
2017-05-02 14:59:09 -0400

    added progressbar

--- upload.py
+++ upload.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 import os
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, flash
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = '/opt/data'
 
 app = Flask(__name__)
+app.secret_key = 'eflex upload page'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
+        print(request.files)
         if 'file' not in request.files:
-            flash('No file part')
+            print('No file part')
             return redirect(request.url)
         file = request.files['file']
         if file.filename == '':
-            flash('No Selected File')
+            print('No Selected File')
             return redirect(request.url)
         if file:
+            print(file.filename)
             filename = secure_filename(file.filename) 
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('upload_file', filename=filename))
