78786408647f4e8903dd5e66b08664d2eac5907b
Mark Parmelee <mparm920@gmail.com>
2017-08-15 23:48:38 -0400

    added tests for endpoints

---
+++ test_upload.py
@@ -0,0 +1,34 @@
+from upload.upload import app
+import unittest
+
+class UploadTest(unittest.TestCase):
+
+    def setup(self):
+        app.config["SECRET_KEY"] = 'eflex upload page'
+
+    def test_Cant_access_root_endpoint(self):
+        self.tester = app.test_client(self)
+        res = self.tester.get('/', follow_redirects=True)
+        self.assertEqual(res.status_code, 200)
+        self.assertIn(b"<label>Login</label>", res.data)
+
+    def test_login_endpoint(self):
+        self.tester = app.test_client(self)
+        res = self.tester.get('/login', follow_redirects=True)
+        self.assertEqual(res.status_code, 200)
+        self.assertIn(b"<label>Login</label>", res.data)
+
+    def test_login_redirect(self):
+        self.tester = app.test_client(self)
+        res = self.tester.post('/login', data=dict(username='admin', password='admin'), follow_redirects=True)
+        self.assertEqual(res.status_code, 200)
+        self.assertIn(b'id="uploadForm"', res.data)
+
+    def test_logout_endpoint(self):
+        self.tester = app.test_client(self)
+        res = self.tester.get('/logout', follow_redirects=True)
+        self.assertEqual(res.status_code, 200)
+        self.assertIn(b"<label>Login</label>", res.data)
+
+if __name__ == '__main__':
+    unittest.main()
