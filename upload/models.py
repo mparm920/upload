a4d79ec16d2b4451aea56020cbdceb3ac44a366c
Mark Parmelee <mparm920@gmail.com>
2017-08-24 09:09:27 -0400

    add user model

--- upload/models.py
+++ upload/models.py
@@ -3,8 +3,17 @@ from upload import db
 import uuid
 
 class Users(db.Model):
-   id = db.Column(db.integer, primary_key=True) 
-   email = db.Column(db.String(), unique=True)
-   password = db.Column(db.String())
-   creationDate = db.Column(db.DateTime())
-   accessDate = db.Column(db.DateTime())
+    
+    __tablename__ = "Users"
+
+    id = db.Column(db.String, primary_key=True, default=str(uuid.uuid4()))
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+    creationDate = db.Column(db.DateTime)
+    accessDate = db.Column(db.DateTime)
+
+    def __init__(self, email, password, creationDate, accessDate):
+        self.email = email
+        self.password = password
+        self.creationDate = creationDate
+        self.accessDate = accessDate
