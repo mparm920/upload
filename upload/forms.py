6d54562d625e26d013cc2489ee5d4b1a8476316b
Mark Parmelee <mparm920@gmail.com>
2017-08-31 16:01:19 -0400

    WIP Register Form

--- upload/forms.py
+++ upload/forms.py
@@ -5,3 +5,11 @@ from wtforms.validators import DataRequired, Email
 class LoginForm(FlaskForm):
     email = TextField('email', validators=[DataRequired(), Email()])
     password = PasswordField('password', validators=[DataRequired()])
+
+class RegisterForm(FlaskForm):
+    email = TextField('email', validators=[DataRequired(), Email()])
+    password = PasswordField('password', validators=[DataRequired(), EqualTo('confirm', message='Passwords must match')])
+    confirm = PasswordField('confirmPassword', validators=[DataRequired()])
+    company = TextField('company', validators=[DataRequired()])
+    location = TextField('location')
+    assemblyLine = TextField('assemblyLine', validators=[DataRequired()])
