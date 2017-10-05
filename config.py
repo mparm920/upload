226e02c36e7fc6603e2137e04e0ac5d9c8bab4f7
Mark Parmelee <mparm920@gmail.com>
2017-10-04 20:12:30 -0400

    hard coded database uri

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class BaseConfig():
     DEBUG = False
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///upload.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////home/mparm920/Code/upload/upload.db'
     UPLOAD_FOLDER = '/Users/mparm920/Code/upload/files/'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
