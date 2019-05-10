b76344ce78f7a66524370789e6e055f8188f62cf
Mark Parmelee <mparm920@pop-os.localdomain>
2019-05-09 20:32:54 -0400

    registration form updates

--- project/user/forms.py
+++ project/user/forms.py
@@ -1,12 +1,13 @@
 
 from flask_wtf import FlaskForm
-from wtforms import TextField, PasswordField
+from wtforms import TextField, PasswordField, SelectField
 from wtforms.validators import DataRequired, Email, EqualTo
+from project.models import Companies
 
 class RegisterForm(FlaskForm):
     email = TextField('email', validators=[DataRequired(), Email()])
     password = PasswordField('password', validators=[DataRequired(), EqualTo('confirm', message='Passwords must match')])
     confirm = PasswordField('confirmPassword', validators=[DataRequired()])
-    company = TextField('company', validators=[DataRequired()])
-    location = TextField('location')
-    assemblyLine = TextField('assemblyLine', validators=[DataRequired()])
+    company = SelectField('company', coerce=int)
+    #location = TextField('location')
+    #assemblyLine = TextField('assemblyLine')
--- project/user/views.py
+++ project/user/views.py
@@ -1,7 +1,7 @@
 from .forms import RegisterForm
-from flask import Blueprint, render_template, request
-from project import app
-from project.models import Companies
+from flask import Blueprint, render_template, request, redirect, url_for
+from project import app, db
+from project.models import Companies, Users
 
 user_blueprint = Blueprint(
     'register', __name__,
@@ -10,9 +10,17 @@ user_blueprint = Blueprint(
 
 @user_blueprint.route('/register', methods=['GET', 'POST'])
 def register_user():
+    companies = Companies.query.all()
+    regForm = RegisterForm(request.form)
+    regForm.company.choices= [(c.id, c.companyName) for c in Companies.query.all()]
     if request.method == 'POST':
-        pass
-    else:
-        registerForm = RegisterForm(request.form)
-        companies = Companies.query.all()
-        return render_template('register.html', form=registerForm, companies=companies)
+        if regForm.validate_on_submit():
+            print('form validated')
+            print(request.form['email'], request.form['password'], request.form['company'])
+            user = Users(request.form['email'], request.form['password'], request.form['company'])
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('login.login'))
+        else:
+            print('form NOT validated')
+    return render_template('register.html', form=regForm, companies=companies)
